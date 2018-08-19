ea9a2f1f334067aa76b0901f7e396dd4764b9785
hjain0726 <hjain0726@gmail.com>
2018-08-19 21:44:44 +0530

    first commit

---
+++ utils/cors.js
@@ -0,0 +1,6 @@
+const cors = (req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  }
+  module.exports= cors;
